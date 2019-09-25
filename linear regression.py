03cb6ec7d7b948202f76a80d637fcdb07ba7f129
ZhuHaizhen <zhuhaizhen1024@163.com>
2019-09-25 22:37:27 +0800

    tested

--- linear regression.py
+++ linear regression.py
@@ -21,28 +21,32 @@ def plot_data(x, y):
     plt.show()
 
 
-data = pd.read_csv('ex1data1.txt', sep=',')
-x0 = np.array(data.iloc[:, 0])
-y0 = np.array(data.iloc[:, 1])
+data = pd.read_csv('ex1data1.txt', sep=',', header=None)
+data = np.array(data)
+print(data.shape)
+
+x0 = data[:, 0][:, np.newaxis]
+y0 = data[:, 1][:, np.newaxis]
 plot_data(x0, y0)
 
 m = np.shape(y0)[0]
-x0_mat = pd.concat([np.ones(m), x0], axis=1, ignore_index=True)
+x0_mat = np.hstack((np.ones((m, 1)), x0))
 
 
 def compute_cost(x_mat, y, theta):
-    j = 1 / (2 * np.shape(x_mat)[0]) * np.sum((theta.T.dot(x_mat) - y) ** 2)
+    j = 1 / (2 * np.shape(x_mat)[0]) * np.sum((x_mat.dot(theta) - y) ** 2)
     return j
 
 
 theta0 = np.zeros((2, 1))
-compute_cost(x0_mat, y0, theta0)
+print(compute_cost(x0_mat, y0, theta0))
+print(compute_cost(x0_mat, y0, np.array([[-1], [2]])))
 
 
 def grad_descent(x_mat, y, theta, alpha, iterations):
     j_history = np.zeros(iterations)
     for i in range(iterations):
-        theta = theta - alpha / np.shape(x_mat)[0] * x_mat.T.dot(theta.T.dot(x_mat) - y)
+        theta = theta - alpha / np.shape(x_mat)[0] * x_mat.T.dot(x_mat.dot(theta) - y)
         j_history[i] = compute_cost(x_mat, y, theta)
 
     return theta, j_history
@@ -51,9 +55,13 @@ def grad_descent(x_mat, y, theta, alpha, iterations):
 iter0 = 1500
 alpha0 = 0.01
 theta1, j_history1 = grad_descent(x0_mat, y0, theta0, alpha0, iter0)
+print(theta1)
+print(j_history1)
 
 plt.subplot(111)
-plt.scatter(x0, y0, 'rx')
-plt.plot(x0, theta1.T.dot(x0), 'b-')
+plt.scatter(x0, y0, color='r', marker='x')
+plt.plot(x0, x0_mat.dot(theta1), 'b-')
 plt.legend(loc=4)
+plt.xlabel('Profit in $ 10,000s')
+plt.ylabel('Population of City in 10,000s')
 plt.show()
