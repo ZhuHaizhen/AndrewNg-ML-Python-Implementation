48439ae10f24fe4899c2c955d24e1415d039d57d
ZhuHaizhen <zhuhaizhen1024@163.com>
2019-09-27 20:20:57 +0800

    linear multi

--- linear_multiple.py
+++ linear_multiple.py
@@ -22,7 +22,8 @@ def feature_norm(x):
 data = np.loadtxt('ex1data2.txt', delimiter=',')
 print(data.shape)
 x0 = data[:, 0:-1]
-y0 = data[:, -1]
+y0 = data[:, -1][:, np.newaxis]
+
 
 x0_norm, mu0, sigma0 = feature_norm(x0)
 m = y0.shape[0]
@@ -30,7 +31,7 @@ x0_mat = np.hstack((np.ones((m, 1)), x0_norm))
 
 
 def compute_cost_multi(x_mat, y, theta):
-    j = 1 / 2 * np.shape(x_mat)[0] * np.sum((x_mat.dot(theta) - y) ** 2)
+    j = 1 / 2 * np.shape(x_mat)[0] * (x_mat.dot(theta) - y).T.dot(x_mat.dot(theta) - y)
 
     return j
 
@@ -43,14 +44,36 @@ print(j0)
 def gradient_descent_multi(x_mat, y, alpha, theta, iterations):
     j_history = np.zeros(iterations)
     for i in range(iterations):
-        theta = theta - alpha * np.shape(x_mat)[0] * x_mat.T.dot(x_mat.dot(theta) - y)
+        theta = theta - alpha / np.shape(x_mat)[0] * x_mat.T.dot(x_mat.dot(theta) - y)
         j_history[i] = compute_cost_multi(x_mat, y, theta)
 
     return theta, j_history
 
 
-alpha0 = 0.01
-iterations0 = 400
+alpha0 = 0.15
+iterations0 = 50
 theta1, j_history1 = gradient_descent_multi(x0_mat, y0, alpha0, theta0, iterations0)
 
 print(theta1)
+
+plt.plot(j_history1, 'b-')
+plt.xlabel('Number of iterations')
+plt.ylabel('Cost J')
+plt.show()
+
+predicted = theta1.T.dot(np.array([[1], [(1650 - mu0[0]) / sigma0[0]], [3 - mu0[1] / sigma0[1]]]))
+print(predicted)
+
+
+def normal_eqn(x_mat, y):
+    theta = np.linalg.pinv(x_mat.T.dot(x_mat)).dot(x_mat.T).dot(y)
+
+    return theta
+
+
+x1_mat = np.hstack((np.ones((m, 1)), x0))
+theta2 = normal_eqn(x1_mat, y0)
+print(theta2)
+
+predicted0 = theta2.T.dot(np.array([[1], [1650], [3]]))
+print(predicted0)
